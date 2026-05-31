3a9750d484986ebc68f063ef3a3501e183377a44
visatk <visatk29@gmail.com>
2026-05-31 19:39:30 +0600

    Update db.ts

--- worker/middleware/db.ts
+++ worker/middleware/db.ts
@@ -1,10 +1,11 @@
 import type { Context, Next } from 'hono';
 import { drizzle } from 'drizzle-orm/d1';
+import * as schema from '../db/schema';
 import type { Bindings, Variables } from '../types';
 
 export const injectDb = async (c: Context<{ Bindings: Bindings, Variables: Variables }>, next: Next) => {
   if (!c.get('db')) {
-    const db = drizzle(c.env.DB);
+    const db = drizzle(c.env.DB, { schema });
     c.set('db', db);
   }
   await next();
